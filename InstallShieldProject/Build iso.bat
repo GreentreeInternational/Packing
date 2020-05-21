b98c79cb448b36ca34822eae62dc1a168512427e
chrispower42 <chris.power@myob.com>
2020-05-22 07:20:50 +1200

    MGT-5086 Rewrote Build iso.bat to put intermediate output in the project's Media\GT3D-DVD directory as used when building the project from InstallShield GUI

--- InstallShieldProject/Build iso.bat
+++ InstallShieldProject/Build iso.bat
@@ -1,7 +1,21 @@
-"C:\Program Files (x86)\InstallShield\2018\System\ISBuild.exe" -p"E:\InstallShieldProject\Greentree Financial Software.ism" -m"GT3D-DVD" -b"e:\LoadNGo\Temp\Build"
-xcopy "e:\LoadNGo\Temp\Build\Disk Images\Disk1" "e:\LoadNGo\GT3D-DVD\Installer" /y
-e:\LoadNGo\bcd\bin\mkisofs -r -l -L -J -o "e:\Latest install DVD iso\Greentree installation CD.iso" "e:\LoadNGo\GT3D-DVD\"
-e:
-cd "e:\Latest install DVD iso"
-e:\LoadNGo\bcd\bin\md5sum.exe -b "Greentree installation CD.iso" > "Greentree installation CD.md5"
+ECHO OFF
+REM This builds the project in its \Media directory then creates iso and md5 in the Latest Install directory
+REM If you've modified scripts you must compile them before building the project
+set project=Greentree Financial Software.ism
+set ISPROJECTDIR=E:\InstallShieldProject
+set media=GT3D-DVD
+set MEDIADIR=%ISPROJECTDIR%\Media\%media%
+set ISODIR=E:\Latest install DVD iso
+set iso=Greentree installation CD.iso
+del "%MEDIADIR%\Disk Images\Disk1\setup.exe"
+ECHO 1. Building the %media% release for live project %ISPROJECTDIR%\%project%
+"C:\Program Files (x86)\InstallShield\2018\System\ISBuild.exe" -p"%ISPROJECTDIR%\%project%" -m"%media%" -b"%MEDIADIR%"
+ECHO 2. Building the iso file %ISODIR%\%iso%
+E:\LoadNGo\bcd\bin\mkisofs -r -l -L -J -o "%ISODIR%\%iso%" "%MEDIADIR%\Disk Images\Disk1\"
+ECHO 3. Building the md5 file
+REM It seems md5sum can't handle a path containing spaces so we do this the clumsy way
+E:
+cd "%ISODIR%"
+E:\LoadNGo\bcd\bin\md5sum.exe -b "%iso%" > "Greentree installation CD.md5"
+dir
 pause
